7841effeb6501cf7fc78384c165bf10179d878e1
Mohankrishna <mohankrishna3516@gmail.com>
2026-07-04 16:20:29 -0500

    Build legacy netshoot tools with patched modules

--- Dockerfile
+++ Dockerfile
@@ -31,7 +31,6 @@ RUN set -ex \
     bridge-utils \
     busybox-extras \
     conntrack-tools \
-    ctop \
     curl \
     dhcping \
     drill \
@@ -70,7 +69,6 @@ RUN set -ex \
     strace \
     tcpdump \
     tcptraceroute \
-    termshark \
     tshark \
     util-linux \
     vim \
@@ -86,8 +84,14 @@ RUN set -ex \
       swaks \
       trippy
 
+# Installing ctop
+COPY --from=fetcher /tmp/ctop /usr/local/bin/ctop
+
 # Installing calicoctl
 COPY --from=fetcher /tmp/calicoctl /usr/local/bin/calicoctl
+
+# Installing termshark
+COPY --from=fetcher /tmp/termshark /usr/local/bin/termshark
 
 # Installing grpcurl
 COPY --from=fetcher /tmp/grpcurl /usr/local/bin/grpcurl
--- build/fetch_binaries.sh
+++ build/fetch_binaries.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+CTOP_VERSION="${CTOP_VERSION:-0.7.7}"
 CALICOCTL_VERSION="${CALICOCTL_VERSION:-v3.32.1}"
+TERMSHARK_VERSION="${TERMSHARK_VERSION:-2.4.0}"
 GRPCURL_VERSION="${GRPCURL_VERSION:-1.9.3}"
 FORTIO_VERSION="${FORTIO_VERSION:-1.75.2}"
 
@@ -15,10 +17,56 @@ case $ARCH in
         ;;
 esac
 
+get_ctop() {
+  VERSION=${CTOP_VERSION#v}
+  git clone --depth 1 --branch "v${VERSION}" https://github.com/bcicen/ctop.git /tmp/ctop-src
+  (
+    cd /tmp/ctop-src
+    go get -u=patch .
+    go mod edit \
+      -require=github.com/fsouza/go-dockerclient@v1.13.2 \
+      -require=github.com/containerd/containerd@v1.7.33 \
+      -require=github.com/opencontainers/image-spec@v1.1.1 \
+      -require=github.com/opencontainers/runtime-spec@v1.3.0 \
+      -require=github.com/opencontainers/runc@v1.5.0 \
+      -require=golang.org/x/crypto@v0.53.0 \
+      -require=golang.org/x/net@v0.56.0 \
+      -require=golang.org/x/sys@v0.46.0 \
+      -require=golang.org/x/text@v0.38.0
+    go mod download
+    CGO_ENABLED=0 go build -mod=mod -trimpath -ldflags="-s -w -buildid=" -o /tmp/ctop .
+  )
+  chmod +x /tmp/ctop
+  chown root:root /tmp/ctop
+}
+
 get_calicoctl() {
   VERSION=$CALICOCTL_VERSION
   LINK="https://github.com/projectcalico/calico/releases/download/${VERSION}/calicoctl-linux-${ARCH}"
   wget "$LINK" -O /tmp/calicoctl && chmod +x /tmp/calicoctl
+}
+
+get_termshark() {
+  VERSION=${TERMSHARK_VERSION#v}
+  git clone --depth 1 --branch "v${VERSION}" https://github.com/gcla/termshark.git /tmp/termshark-src
+  (
+    cd /tmp/termshark-src
+    go get -u=patch ./cmd/termshark
+    go mod edit \
+      -require=github.com/antchfx/xmlquery@v1.5.1 \
+      -require=github.com/antchfx/xpath@v1.3.6 \
+      -require=github.com/gin-gonic/gin@v1.12.0 \
+      -require=github.com/sirupsen/logrus@v1.9.4 \
+      -require=github.com/spf13/viper@v1.21.0 \
+      -require=golang.org/x/crypto@v0.53.0 \
+      -require=golang.org/x/net@v0.56.0 \
+      -require=golang.org/x/sys@v0.46.0 \
+      -require=golang.org/x/text@v0.38.0
+    go mod download
+    CGO_ENABLED=0 go build -mod=mod -trimpath -ldflags="-s -w -buildid=" -o /tmp/termshark ./cmd/termshark
+  )
+  chmod +x /tmp/termshark
+  chown root:root /tmp/termshark
 }
 
 get_grpcurl() {
@@ -56,7 +104,9 @@ get_fortio() {
 }
 
 
+get_ctop
 get_calicoctl
+get_termshark
 get_grpcurl
 get_fortio
 
